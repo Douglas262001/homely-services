35fa246e459d2fdcb181b03024632783743d5e60
EduardoDandolini <eduardodasilvadandolini@hmail.com>
2024-01-23 21:17:10 -0300

    Initial commit

---
+++ src/main/java/com/team/homelyservices/HomelyServicesApplication.java
@@ -0,0 +1,13 @@
+package com.team.homelyservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomelyServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomelyServicesApplication.class, args);
+	}
+
+}
